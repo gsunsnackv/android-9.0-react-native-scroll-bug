5ac3b96bd5a299e677df5242a3083a6a0def4825
Gordon Sun <gsun@snackv.com>
2019-01-18 01:02:27 -0800

    Show images works.

--- App.js
+++ App.js
@@ -162,33 +162,10 @@ export default class RecycleTestComponent extends React.Component {
       //You'll need data in most cases, we don't provide it by default to enable things like data virtualization in the future
       //NOTE: For complex lists LayoutProvider will also be complex it would then make sense to move it to a different file
       this._layoutProvider = new LayoutProvider(
-          index => {
-              if (index % 3 === 0) {
-                  return ViewTypes.FULL;
-              } else if (index % 3 === 1) {
-                  return ViewTypes.HALF_LEFT;
-              } else {
-                  return ViewTypes.HALF_RIGHT;
-              }
-          },
+          index => 'image',
           (type, dim) => {
-              switch (type) {
-                  case ViewTypes.HALF_LEFT:
-                      dim.width = width / 2;
-                      dim.height = 160;
-                      break;
-                  case ViewTypes.HALF_RIGHT:
-                      dim.width = width / 2;
-                      dim.height = 160;
-                      break;
-                  case ViewTypes.FULL:
-                      dim.width = width;
-                      dim.height = 140;
-                      break;
-                  default:
-                      dim.width = 0;
-                      dim.height = 0;
-              }
+            dim.width = 300;
+            dim.height = 300;
           }
       );
 
@@ -196,7 +173,7 @@ export default class RecycleTestComponent extends React.Component {
 
       //Since component should always render once data has changed, make data provider part of the state
       this.state = {
-          dataProvider: dataProvider.cloneWithRows(this._generateArray(300))
+          dataProvider: dataProvider.cloneWithRows(images)
       };
   }
 
@@ -210,29 +187,10 @@ export default class RecycleTestComponent extends React.Component {
 
   //Given type and data return the view component
   _rowRenderer(type, data) {
-      //You can return any view here, CellContainer has no special significance
-      switch (type) {
-          case ViewTypes.HALF_LEFT:
-              return (
-                  <CellContainer style={styles.containerGridLeft}>
-                      <Text>Data: {data}</Text>
-                  </CellContainer>
-              );
-          case ViewTypes.HALF_RIGHT:
-              return (
-                  <CellContainer style={styles.containerGridRight}>
-                      <Text>Data: {data}</Text>
-                  </CellContainer>
-              );
-          case ViewTypes.FULL:
-              return (
-                  <CellContainer style={styles.container}>
-                      <Text>Data: {data}</Text>
-                  </CellContainer>
-              );
-          default:
-              return null;
-      }
+    //You can return any view here, CellContainer has no special significance
+    return (
+      <Image style={{ width: 300, height: 300}} source={{ uri: data }} />
+    );
   }
 
   render() {
